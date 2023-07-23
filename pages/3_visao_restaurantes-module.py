12743cc27e72a6928c980a5b48aac1ef4f35aedc
Felipe Moura <102359352+fmooura@users.noreply.github.com>
2023-07-23 10:49:45 -0300

    Update 3_visao_restaurantes-module.py

--- pages/3_visao_restaurantes-module.py
+++ pages/3_visao_restaurantes-module.py
@@ -123,9 +123,9 @@ st.sidebar.markdown("""---""")
 st.sidebar.markdown('## Selecione uma data limite')
 date_slider = st.sidebar.slider(
     'Até qual valor?',
-    value=pd.to_datetime(2022, 4, 13, errors='coerce'),
-    min_value=pd.to_datetime(2022, 2, 11, errors='coerce'),
-    max_value=pd.to_datetime(2022, 4, 6, errors='coerce'),
+    value=pd.to_datetime("2022-04-13", errors='coerce'),
+    min_value=pd.to_datetime("2022-02-11", errors='coerce'),
+    max_value=pd.to_datetime("2022-04-06", errors='coerce'),
     format='DD-MM-YYYY')
 
 
